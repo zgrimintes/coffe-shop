5cbbe16b0b7025a511be3bb0d392510672f7b50b
Tibirean Petru <peptib07@gmail.com>
2025-04-25 13:14:27 +0300

    initial commit

---
+++ src/app/_Components/coffee-card/coffee-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-coffee-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './coffee-card.component.html',
+  styleUrl: './coffee-card.component.scss'
+})
+export class CoffeeCardComponent {
+
+}
